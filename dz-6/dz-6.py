97fd60e3e310c6475ab214d1ffefdfee61329106
dn200984cda <dn200984cda@gmail.com>
2024-11-23 17:48:46 +0100

    modified:   dz-6/dz-6.py

--- dz-6/dz-6.py
+++ dz-6/dz-6.py
@@ -2,6 +2,7 @@ import re
 import requests
 import sqlite3
 import json
+from lxml import etree
 
 from bs4 import BeautifulSoup
 
@@ -24,10 +25,57 @@ def bs_parse():
         content = f.read()
 
     soup = BeautifulSoup(content, 'lxml')
-    news = soup.find_all('div', class_=re.compile('.*HierachichalCollectionsWrapper.*'))
-    print(news[0])
+    
+    
+    # url
+    # //div[contains(@class, 'HierachichalCollectionsWrapper')]//div[contains(@class, 'Stack')]/a[not(contains(@href, 'comments'))]
+
+    # topics
+    # //div[contains(@class, 'TopicListWrapper')]//a
+    # tree = etree.HTML(content)
+    # xpath = "//div[contains(@class, 'HierachichalCollectionsWrapper')]//div[contains(@class, 'Stack')]/a[contains(@href, 'articles')]"
+    # links = tree.xpath(xpath)
+    # for i, article in enumerate(links, 1):
+    #        article_content = etree.tostring(article, encoding='unicode', pretty_print=True)
+    #        print(article_content)
+
+    news = soup.find('div', class_=re.compile('HierachichalCollectionsWrapper'))
+    #print(news)
+    cards = news.find_all('div', class_=re.compile('.*Stack.*'))
+    print(cards.__len__())
+    results = []
+    for card in cards:
+        url = card.find('a').get('href')
+        directLink = 'https://www.bbc.com' + url
+        #print(directLink)
+        parse_page(directLink)
+        results.append(parse_page(directLink))
+
+    write_json(results)
 
 
+def parse_page(url: str) -> dict:
+    #print(url)
+    response = requests.get(
+        url,
+        headers={
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
+        }
+        )
+    soup = BeautifulSoup(response.text, 'lxml')
+    # topics
+    # //div[contains(@class, 'TopicListWrapper')]//a
+    topicList =[]
+    try:
+        topics = soup.find('div', class_=re.compile('.*TopicListWrapper.*')).find_all('a')
+        for topic in topics:
+            topicList.append(topic.text)
+    except:
+        None
+
+    return {'url': url, 'topics': topicList}
+
+    
 def parse_jobs():
     with open('dz-4-content', 'r', encoding='utf-8') as f:
         content = f.read()
@@ -105,9 +153,8 @@ def write_json(data: list) -> None:
 
     data = [
         {
-            'title': item['title'],
-            'url': item['url']
-
+            'url': item['url'],
+            'topics': item['topics']
         }
         for item in data
     ]
@@ -119,6 +166,9 @@ def write_json(data: list) -> None:
 
 if __name__ == "__main__":
     bs_parse()
+
+    #print(parse_page('https://www.bbc.com/sport/rugby-union/articles/c1wjynv81nwo'))
+
     #get_content()
     #jobs = parse_jobs()
     
